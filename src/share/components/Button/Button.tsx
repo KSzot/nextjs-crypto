399d5936b4c9e64792acfdc1db03ee88c28796f0
Krystian Szot <k.szot@nextrope.com>
2023-07-15 18:32:24 +0200

    fix(config): set eslint prettier airbnb husky

--- src/share/components/Button/Button.tsx
+++ src/share/components/Button/Button.tsx
@@ -5,12 +5,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'primary' | 'gold' | 'outline';
 }
 
-export const Button = ({ children, variant, ...props }: ButtonProps) => {
+export const Button = ({ children, variant, type, ...props }: ButtonProps) => {
   const className = `${styles.custom_button} ${
     styles[`custom_button__${variant}`]
   }`;
 
   return (
+    // eslint-disable-next-line react/button-has-type
     <button className={className} {...props}>
       {children}
     </button>
